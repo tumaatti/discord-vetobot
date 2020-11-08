f8a0fdcdac7b3ccb6b4fcd21c262ef57f5bfbeb4
Tuomo <tuomo.vidberg@gmail.com>
2020-11-08 14:37:36 +0200

    initial commit

---
+++ vetobot.py
@@ -0,0 +1,56 @@
+import os
+import random
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+description = "A bot"
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(
+    command_prefix='!',
+    description=description,
+    intents=intents
+)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as', bot.user)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@bot.command()
+async def veto(ctx, *args):
+    if len(args) > 5:
+        await ctx.send('too many players')
+        return
+    argsl = list(args)
+    message = (
+        '```\n'
+        'Maps: Anubis, Cache, Dust2, Inferno, Mirage, Nuke, Train, Vertigo\n\n'
+        'Veto order:\n'
+    )
+    for i in range(len(argsl)):
+        vetoer = argsl.pop(random.randint(0, len(argsl) - 1))
+        if i < len(args) - 3:
+            message += f'Ban:  {vetoer}\n'
+        else:
+            message += f'Pick: {vetoer}\n'
+    message += '```'
+
+    await ctx.send(message)
+
+
+bot.run(TOKEN)
